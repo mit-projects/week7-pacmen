ab4c814fc7bc808bd6a28a224b9e8546ff2560a8
Nick Bernal <nickjbernal@gmail.com>
2022-04-14 18:49:53 -0700

    Updated into a game

--- pacmen.js
+++ pacmen.js
@@ -2,6 +2,8 @@ const pacArray = [
   ['./images/PacMan1.png', './images/PacMan2.png'],
   ['./images/PacMan3.png', './images/PacMan4.png'],
 ];
+let mouseX = 0;
+let mouseY = 0;
 
 // This function returns an object with random values
 function setToRandom(scale) {
@@ -19,22 +21,22 @@ class pacMan {
     velocity = 0;
 
     constructor() {
-        console.log('new pac');
-        this.position = setToRandom(200);
-        this.velocity = setToRandom(10);
+      this.position = setToRandom(200);
+      this.velocity = setToRandom(10);
 
-        this.img.style.position = 'absolute';
-        this.img.src = pacArray[0][0];
-        this.img.width = 50;
-        this.img.style.left = this.position.x;
-        this.img.style.top = this.position.y;
-        setInterval(() => this.animate(), 500);
-        setInterval(() => this.move(), 20);
+      this.img.style.position = 'absolute';
+      this.img.classList.add('pacman');
+      this.img.src = pacArray[0][0];
+      this.img.width = 50;
+      this.img.style.left = this.position.x;
+      this.img.style.top = this.position.y;
+      setInterval(() => this.animate(), 500);
+      setInterval(() => this.move(), 20);
     }
     animate() {
-        !this.mouthPos ? this.mouthPos = 1 : this.mouthPos = 0;
-        if (this.velocity.x < 0) this.img.src = pacArray[1][this.mouthPos];
-        else this.img.src = pacArray[0][this.mouthPos];
+      !this.mouthPos ? this.mouthPos = 1 : this.mouthPos = 0;
+      if (this.velocity.x < 0) this.img.src = pacArray[1][this.mouthPos];
+      else this.img.src = pacArray[0][this.mouthPos];
     }
     move() {
       this.checkCollisions();
@@ -45,6 +47,11 @@ class pacMan {
     }
 
     checkCollisions() {
+      if (mouseX > this.position.x && mouseX < this.position.x + this.img.width) {
+        if (mouseY > this.position.y && mouseY < this.position.y + this.img.height) {
+          this.destroy();
+        }
+      }
       if(this.position.x >= window.innerWidth - this.img.width - this.velocity.x  || this.position.x <= 0) {
         this.velocity.x *= -1;
         if (this.velocity.x < 0) this.img.src = pacArray[1][this.mouthPos];
@@ -54,6 +61,13 @@ class pacMan {
         this.velocity.y *= -1;
       }
     }
+
+    destroy() {
+      this.img.classList.add('pacDeath');
+      setTimeout(() => {
+        this.img.remove();
+      }, 600)
+    }
 }
 
 // Factory to make a PacMan at a random position with random velocity
@@ -61,4 +75,13 @@ function makeOne() {
     let game = document.getElementById('game');
     p = new pacMan();
     game.appendChild(p.img);
+}
+function main() {
+
+}
+window.onload = main();
+
+onmousemove = function(e) { 
+  mouseX = e.clientX;
+  mouseY = e.clientY;
 }
